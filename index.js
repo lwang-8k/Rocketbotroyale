4c5a3a83cfe3b21483273acc07ace3e9e9dab637
lwang-8k <800026388@fjuhsd.org>
2024-10-25 14:02:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const express = require("express")
 const https = require("https")
 const app = express()
-
-app.get("/chess/*", (req, res, next) => {
-  const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
 app.get("/*", (req, res, next) => {
-  //const pathSegments = req.path.split('/');
- // const rocketPath = pathSegments[2]
   https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
